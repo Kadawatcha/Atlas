c8d77f6575a90b280ce51d8fe8d55355c15bf1df
Kadawatcha <163724568+Kadawatcha@users.noreply.github.com>
2026-05-31 16:23:37 +0200

    version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
     defaultConfig {
         applicationId = "com.kadawatcha.app"
         minSdk = 26
-        versionCode = 4
-        versionName = "1.6.2"
+        versionCode = 5
+        versionName = "1.6.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
