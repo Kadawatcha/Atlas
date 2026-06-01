404529f416a65ff685f9382396f9da163a83fc1e
Kadawatcha <163724568+Kadawatcha@users.noreply.github.com>
2026-06-01 21:18:35 +0200

    updated DropdownMenu styling and added icons to menu items

--- app/src/main/java/com/kadawatcha/app/ui/MainScreen.kt
+++ app/src/main/java/com/kadawatcha/app/ui/MainScreen.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.filled.ChatBubbleOutline
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.LocalFireDepartment
@@ -18,6 +19,7 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
@@ -36,6 +38,7 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -158,19 +161,33 @@ private fun MainTopMenu(onLogoutClick: () -> Unit) {
         DropdownMenu(
             expanded = expanded,
             onDismissRequest = { expanded = false },
-            shape = RoundedCornerShape(10.dp),
-            border = BorderStroke(1.dp, MaterialTheme.colorScheme.outlineVariant)
+            shape = RoundedCornerShape(8.dp),
+            // border = BorderStroke(1.dp, MaterialTheme.colorScheme.outlineVariant)
         ) {
             DropdownMenuItem(
                 text = { Text("Settings") },
-                onClick = { expanded = false }
+                onClick = { expanded = false },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.Settings,
+                        contentDescription = null
+                    )
+                }
             )
+
+            HorizontalDivider()
 
             DropdownMenuItem(
                 text = { Text("Logout") },
                 onClick = {
                     expanded = false
                     onLogoutClick()
+                },
+                leadingIcon = {
+                    Icon(
+                        imageVector = Icons.Default.ArrowBackIosNew,
+                        contentDescription = null
+                    )
                 }
             )
         }
