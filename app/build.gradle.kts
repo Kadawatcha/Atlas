df6dd6bf0a8dadca5f54c96e8d89894484af40da
Kadawatcha <163724568+Kadawatcha@users.noreply.github.com>
2026-09-20 19:46:39 +0200

    nom

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ android {
         minSdk = 26
         targetSdk = 37
         versionCode = 10
-        versionName = "1.14.0"
+        versionName = "1.15.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
